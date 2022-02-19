72cb1ec663e5a64c02e78e5dfc74d92b5cb17e75
Raul G <fasthands776@gmail.com>
2022-02-19 23:38:48 +0900

    started post components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {PostCard, Categories, PostWidget} from '../components'
 
+const posts = [
+  {title: 'Blog Test', excerpt: 'This is a test post'},
+  {title: 'Blog Test 2', excerpt: 'This is a test post 2'},
+]
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS blog</title>
+        <link rel="icon" href="#"></link>
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+
+
+
+    </div>
   )
 }
